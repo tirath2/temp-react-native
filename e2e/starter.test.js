170e8831cd3a42853be37e4d70ab6cc5918d956e
Tirath <tirath@groyyo.com>
2023-01-16 17:13:54 +0530

    test

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -10,16 +10,9 @@ describe('Example', () => {
 
   it('auth', async () => {
     await waitFor(element(by.id('loginMobileInput'))).toBeVisible();
-    await element(by.id('loginMobileInput')).typeText('789789789');
-    await waitFor(element(by.id('loginPasswordInput'))).toBeVisible();
-    await element(by.id('loginPasswordInput')).typeText('abcdef');
-    await waitFor(element(by.id('loginButton'))).toBeVisible();
-    await element(by.id('loginButton')).longPress();
-    await expect(element(by.text('Number is invalid'))).toBeVisible();
-
-    await waitFor(element(by.id('loginMobileInput'))).toBeVisible();
     await element(by.id('loginMobileInput')).replaceText('7897897890');
-
+    await waitFor(element(by.id('loginPasswordInput'))).toBeVisible();
+    await element(by.id('loginPasswordInput')).replaceText('abcdef');
     await waitFor(element(by.id('loginButton'))).toBeVisible();
     await element(by.id('loginButton')).longPress();
     await waitFor(element(by.text('Home'))).toBeVisible();
